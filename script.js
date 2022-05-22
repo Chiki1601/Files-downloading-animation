2509d38a8d712fc95430d6cc6fa1f9fbe637dfb3
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-22 09:25:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+/*
+ * ICONS FROM ICONFINDER.COM
+ *
+ * folder: https://www.iconfinder.com/icons/416376/envelope_files_folder_interface_office_icon
+ * 
+ * file: https://www.iconfinder.com/icons/143818/file_icon#size=128
+ *
+ * server: https://www.iconfinder.com/icons/379362/cloud_database_icon
+ */
